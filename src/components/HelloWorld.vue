cd96adc0fc6d6cb171bdfb3cb0a7bbe4b50abdcf
BohdanIllishyk <biluybodia@gmail.com>
2019-03-24 22:42:42 +0200

    add new project

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,102 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+    <div>
+        <form @submit.prevent="submit">
+            <div class="form-step-one" v-show="step === 1">
+                <input type="text" name="firstName" id="firstName" v-model.trim="$v.firstName.$model" class="form-control" :class="{ 'is-invalid': $v.firstName.$error }" />
+                <div v-if="submitted && !$v.firstName.required" class="invalid-feedback">First Name is required</div>
+                <input type="submit">
+                {{$v.firstName}}
+
+                <div class="navigation">
+                    <button @click="nextStep()"></button>
+                </div>
+            </div>
+
+            <div class="form-step-one" v-show="step === 2">
+                <input type="text" name="firstName" id="firstName" v-model.trim="$v.firstName.$model" class="form-control" :class="{ 'is-invalid': $v.firstName.$error }" />
+                <div v-if="submitted && !$v.firstName.required" class="invalid-feedback">First Name is required</div>
+                <input type="submit">
+                {{$v.firstName}}
+
+                <div class="navigation">
+                    <button @click="nextStep()"></button>
+                </div>
+            </div>
+
+        </form>
+    </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+    import axios from 'axios'
+    import Vue from 'vue'
+    import { required, between } from "vuelidate/lib/validators"
+    import Vuelidate from 'vuelidate'
+    Vue.use(Vuelidate);
+
+    export default {
+        name: 'HelloWorld',
+        props: {
+            msg: String
+        },
+        data() {
+            return {
+                step: 1,
+                firstName: '',
+                submitted: false
+            }
+        },
+        validations: {
+            firstName: {
+                required,
+                between: between(20, 30)
+            },
+
+        },
+        methods: {
+            submit() {
+                this.submitted = true;
+                // this.$v.form.$touch();
+                // console.log(this.validation)\
+                if (this.$v.$invalid) {
+                    return;
+                }
+
+                alert("SUCCESS!! :-)\n\n" + JSON.stringify(this.firstName));
+            },
+            nextStep() {
+                this.step ++
+            }
+        },
+
+        mounted() {
+            axios
+                .get('validation.json')
+                .then(response => (this.validation = response))
+                .catch(function (error) {
+                    console.log(error)
+                })
+        }
+    }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+    h3 {
+        margin: 40px 0 0;
+    }
+
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+
+    li {
+        display: inline-block;
+        margin: 0 10px;
+    }
+
+    a {
+        color: #42b983;
+    }
 </style>
---
+++ src/components/NavigationButton.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "NavigationButton"
+    }
+</script>
+
+<style scoped>
+
+</style>
